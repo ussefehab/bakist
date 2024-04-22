0bdbf0f9f7d3d795923be31968167f1aa8898c6c
youssefehab25 <123908594+youssefehab25@users.noreply.github.com>
2024-04-22 11:31:57 +0200

    sorting

--- script.js
+++ script.js
@@ -61,10 +61,12 @@ const inputClosePin = document.querySelector('.form__input--pin');
 
 ///////////////////////////////////////////////
 
-const displayMovements = function (movements) {
+const displayMovements = function (movements, sort = false) {
   containerMovements.innerHTML = '';
 
-  movements.forEach(function (mov, i) {
+  const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+  movs.forEach(function (mov, i) {
     const type = mov > 0 ? 'deposit' : 'withdrawal';
     const html = `
     <div class="movements__row">
@@ -226,4 +228,12 @@ btnClose.addEventListener('click', function (e) {
   }
 
   inputCloseUsername.value = inputClosePin.value = '';
+});
+
+// sorting
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+  e.preventDefault();
+  displayMovements(currentAccount.movements, !sorted);
+  sorted = !sorted;
 });
