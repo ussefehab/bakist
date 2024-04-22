b7b5ae528e6744a453a83ea256cedb545053f803
youssefehab25 <123908594+youssefehab25@users.noreply.github.com>
2024-04-22 10:47:37 +0200

    validation

--- script.js
+++ script.js
@@ -125,3 +125,33 @@ const createUsernames = function (accs) {
   });
 };
 createUsernames(accounts);
+
+///// validation
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+  // Prevent form from submitting
+  e.preventDefault();
+
+  currentAccount = accounts.find(
+    acc => acc.username === inputLoginUsername.value
+  );
+  console.log(currentAccount);
+
+  if (currentAccount?.pin === Number(inputLoginPin.value)) {
+    // Display UI and message
+    labelWelcome.textContent = `Welcome back, ${
+      currentAccount.owner.split(' ')[0]
+    }`;
+    containerApp.style.opacity = 100;
+
+    // Clear input fields
+    inputLoginUsername.value = inputLoginPin.value = '';
+    inputLoginPin.blur();
+
+    // Update UI
+    calcDisplayBalance(account1.movements);
+    displayMovements(account1.movements);
+    calcDisplaySummary(account1.movements);
+  }
+});
